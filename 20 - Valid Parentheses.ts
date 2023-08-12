ab9012ed75167ca99260b0be7f79708f1e707dc9
Daniel Li <zaoster777@gmail.com>
2023-08-11 19:42:43 -0500

    valid parentheses solution

---
+++ 20 - Valid Parentheses.ts
@@ -0,0 +1,50 @@
+// Beats 5.4% Runtime, beats 5.12% Memory
+function isValid(s: string): boolean {
+  if (s.length === 1) {
+    // If there's only 1 paren in the string, then it is not a valid parentheses
+    return false;
+  }
+
+  let stack = [];
+
+  for (let el of s) {
+    console.log(el);
+    switch (
+      el // Wouldn't work with || operator, you have to create individual cases for (,[, and {
+    ) {
+      case "(":
+      case "[":
+      case "{":
+        stack.push(el);
+        break;
+      case ")":
+        let paren = stack.pop();
+        if (paren != "(") {
+          console.log(paren);
+          return false;
+        }
+        break;
+      case "]":
+        let bracket = stack.pop();
+        if (bracket != "[") {
+          console.log("bracket");
+          return false;
+        }
+        break;
+      case "}":
+        let curly = stack.pop();
+        if (curly != "{") {
+          console.log("curly");
+          return false;
+        }
+    }
+    console.log(stack);
+  }
+
+  if (stack.length > 0) {
+    // At this point the only way it could still return true and not be valid is if there are just multiple beginning parentheses and we never encountered a closing paren. The length of stack must be 0 or else it's not a valid paren
+    return false;
+  }
+
+  return true; // If after going through all the elements in string we didn't return false, then the parentheses are valid
+}
