69a231f2d0fb819585098041886b1ef901942d68
Daniel Li <zaoster777@gmail.com>
2023-08-21 09:32:55 -0500

    Palindrome Permutation

---
+++ hackerEarth - Palindrome Permutation.ts
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const checkPalindrome = (line: string): void => {
+  let odd = 0;
+  let hashMap = new Map();
+  for (let i = 0; i < line.length; i++) {
+    if (hashMap.has(line[i])) {
+      let prev = hashMap.get(line[i]);
+      hashMap.set(line[i], prev + 1);
+    } else {
+      hashMap.set(line[i], 1);
+    }
+  }
+
+  hashMap.forEach((val, key) => {
+    if (val % 2 === 1) {
+      odd += 1;
+    }
+  });
+
+  if (odd > 1) {
+    process.stdout.write("NO");
+  } else {
+    process.stdout.write("YES");
+  }
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+rl.on("line", (line) => {
+  checkPalindrome(line);
+});
+
+rl.once("close", () => {
+  // end of input
+});
