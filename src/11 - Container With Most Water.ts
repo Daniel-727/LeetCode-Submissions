2f99e2bebe39deea13bccaff0e3c7cf6e8a5e395
Daniel Li <zaoster777@gmail.com>
2023-10-11 21:23:09 -0500

    solved container with most water

---
+++ src/11 - Container With Most Water.ts
@@ -0,0 +1,53 @@
+// Brute force O(n)squared solution, time limit exceeded, so not entirely sure if this solution
+/* function maxArea(height: number[]): number {
+
+    let max = 0;
+
+    for (let i = 0; i < height.length; i++){
+        for (let j = i + 1; j < height.length; j++){
+            let distance = j - i;
+            let tempMax;
+            if (height[i] <= height[j]){
+                tempMax = height[i] * distance;
+            } else {
+                tempMax = height[j] * distance;
+            }
+
+            if (tempMax > max){
+                max = tempMax;
+            }
+        }
+    }
+
+    return max;
+}; */
+
+// better O(n) solution using pointers on the opposite ends of the array
+function maxArea(height: number[]): number {
+  let left = 0;
+  let right = height.length - 1;
+  let max = 0;
+
+  while (left < right) {
+    let lVal = height[left];
+    let rVal = height[right];
+
+    if (lVal <= rVal) {
+      let tempMax = lVal * (right - left);
+      console.log(tempMax);
+      if (tempMax > max) {
+        max = tempMax;
+      }
+      left++;
+    } else {
+      let tempMax = rVal * (right - left);
+      console.log(tempMax);
+      if (tempMax > max) {
+        max = tempMax;
+      }
+      right--;
+    }
+  }
+
+  return max;
+}
